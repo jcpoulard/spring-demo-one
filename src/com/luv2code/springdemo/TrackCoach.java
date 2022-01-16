45f431ff626ae6132999fd68a8f2c9766a991993
poulardjc <jcpoulard@gheskio.org>
2022-01-16 13:28:28 -0500

    Spring dependency injection suite

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,6 +1,12 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService; 
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -10,8 +16,8 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Just Do It: "+fortuneService.getFortune();
 	}
 
 }
